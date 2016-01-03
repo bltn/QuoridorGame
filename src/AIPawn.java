80ed59eb882c201faa28f65b27c02b710660d09d
jordan-bird <birdj1@aston.ac.uk>
2016-01-03 16:41:33 +0000

    Create AIPawn.java

---
+++ src/AIPawn.java
@@ -0,0 +1,27 @@
+
+/**
+ * Instantiable NPC Pawn which is controlled by programmed logic
+ * 
+ * @author Jordan Bird
+ * @version 2/11/2015
+ */
+public class AIPawn extends Pawn
+{
+
+    /**
+     * Constructor for objects of class AIPawn
+     */
+    public AIPawn(int posX, int posY, int walls, String name)
+    {
+        super(posX, posY, walls, name);
+    }
+
+    /**
+     * Causes the AI Pawn to think, and make a decision
+     * 
+     */
+    public void think()
+    {
+        //TODO
+    }
+}
