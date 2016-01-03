a3c1db5ef4f3de90ec2a86911f0ded2e84364949
jordan-bird <birdj1@aston.ac.uk>
2016-01-03 16:46:57 +0000

    Create EasyBrain.java

---
+++ src/EasyBrain.java
@@ -0,0 +1,24 @@
+
+/**
+ * Models an AI brain at a simple difficulty
+ * 
+ * @author Jordan Bird
+ * @version  3/1/2016
+ */
+public class EasyBrain extends Brain
+{
+    /**
+     * Constructor for objects of class EasyBrain
+     */
+    public EasyBrain()
+    {
+
+    }
+
+    /**
+     * Models the decision making process 
+     */
+    public void makeDecision()
+    {
+    }
+}
