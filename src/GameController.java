26c7bcf438cd8abc3794b742399a2881c61ff682
Ben Lawton <ben@scrybstarcopywriting.com>
2016-01-13 01:58:39 +0000

    Further implement GameController

--- src/GameController.java
+++ src/GameController.java
@@ -1,55 +1,123 @@
+import java.util.Iterator;
+
 
 /**
- *
- * @author Khadija
+ * @author Ben Lawton
+ * @author Khadija Patel
  */
-public class GameController {
+public class GameController<T> {
 
     private Board board;
-    private HumanPawn player1;
-    private HumanPawn player2;
-    private HumanPawn currentPlayer;
+    private BoardGUI gui; 
+    
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    
     private int player1WallCount;
     private int player2WallCount;
     private int player1MoveCount;
     private int player2MoveCount;
 
-    public GameController() {
-        board = new Board("player1", "player2");
-        player1 = new HumanPawn(5, 0, 10, "player1");
-        player2 = new HumanPawn(5, 9, 10, "player2");
+    public GameController(BoardGUI gui, Board board) {
+        this.board = board;
+        this.gui = gui;
+        
+        player1 = new Player(5, 0);
+        player2 = new Player(5, 9); 
         currentPlayer = player1;
+        
+        player1WallCount = 10;
+        player2WallCount = 10;
+        player1MoveCount = 0;
+        player2MoveCount = 0;
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args) {}
 
     public void showCurrentPlayerMoves() {
+    	Position position = currentPlayer.getPosition(); 
+    	Position[] availablePositions = board.getOccupiablePositions(position);
+    	if (availablePositions.length > 0) {
+    		for (Position pos : availablePositions) {
+    			//highlightPositionAvailability(pos.getX(), pos.getY()); 
+    		}
+    	}
+    	changePlayer(); 
     }
 
-    public void changePlayer() {
+    public void placeWall(int pos1X, int pos1Y, int pos1Border, int pos2X, int pos2Y, int pos2Border) {
+    	Position position1 = board.getPosition(pos1X, pos1Y);
+    	Position position2 = board.getPosition(pos2X, pos2Y);
+    	
+    	assignWall(position1, pos1Border);
+    	assignWall(position2, pos2Border); 
+    	
+    	decrementPlayerWallCount();
+    	changePlayer();
     }
-
-    public void placeWall(int x, int y) {
+    
+    public Player getCurrentPlayer() {
+    	return currentPlayer; 
     }
 
     public void movePawn(int posX, int posY) {
+    	if (currentPlayer == player1) {
+    		player1.setX(posX);
+    		player1.setY(posY);
+    		player1MoveCount++;
+    		//updatePlayer1MoveCount(player1MoveCount);
+    		//updatePlayer1PawnPosition(player1.getX(), player1.getY());
+    		changePlayer();
+    	}
+    	else {
+    		player2.setX(posX);
+    		player2.setY(posY); 
+    		player2MoveCount++;
+    		//updatePlayer2MoveCount(player2MoveCount);
+    		//updatePlayer2PawnPosition(player2.getX(), player2.getY()); 
+    		changePlayer();
+    	}
     }
 
-    public void decrementPlayer1WallCount() {
-    }
-
-    public void decrementPlayer2WallCount() {
-    }
-
-    public void gameOver(HumanPawn player) {
+    public void gameOver(Player player) {
     }
 
     public void endGame() {
+    }
+    
+    private void assignWall(Position position, int borderValue) {
+    	if (borderValue == -1) {
+    		position.setLeftWall();
+    	}
+    	else if (borderValue == 0) {
+    		position.setTopWall();
+    	}
+    	else if (borderValue == 1) {
+    		position.setRightWall();
+    	}
+    	else if (borderValue == 2) {
+    		position.setBottomWall();
+    	}
+    }
+    
+    private void decrementPlayerWallCount() {
+		if (currentPlayer == player1) {
+			player1WallCount--;
+		}
+		else {
+			player2WallCount--;
+		}
+    }
+    
+    private void changePlayer() {
+    	if (this.currentPlayer == player1) {
+    		currentPlayer = player2;
+    	}
+    	else {
+    		currentPlayer = player1;
+    	}
+    	//updateCurrentPlayer(currentPlayer); 
     }
 
 }
