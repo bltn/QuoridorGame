39c8eec8f5bced88d34aa18f1b88c5d109b36bc4
Ben Lawton <ben@scrybstarcopywriting.com>
2016-02-06 18:14:07 +0000

    Refactor tests

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -5,32 +5,29 @@ import org.junit.Test;
 public class PlayerTest {
 
 	Player aCon = new Player(2,5);
-	
+
 	@Test
-	public void getXTest(){		
-		assertEquals(2, aCon.getX());		
+	public void getXTest(){
+		assertEquals(2, aCon.getX());
 	}
-	
+
 	@Test
-	public void getYTest(){		
-		assertEquals(2, aCon.getY());		
+	public void getYTest(){
+		assertEquals(2, aCon.getY());
 	}
-	
+
 	@Test
 	public void getWallCountest() {
-				
+
 		assertEquals(2, aCon.getX());
 		assertEquals(5, aCon.getY());
-		
-		aCon.decrementWallCount();		
+
+		aCon.decrementWallCount();
 		assertEquals(9, aCon.getWallCount());
-		
-		
-		
 	}
-	
+
 	public void getMoveCountTest(){
-		
+
 		int move = aCon.getMoveCount();
 	    aCon.incrementMoveCount();
 		assertEquals(move+1, aCon.getMoveCount());
@@ -39,7 +36,7 @@ public class PlayerTest {
 		aCon.incrementMoveCount();
 		aCon.incrementMoveCount();
 		assertEquals(move+4, aCon.getMoveCount());
-		
+
 	}
 
 }
