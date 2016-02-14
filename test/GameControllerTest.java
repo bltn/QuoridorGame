8c48536ecde645eda6e904cf36b797b054c17da9
tronyeu0802 <tronyeu0000@gmail.com>
2016-02-14 16:04:29 +0000

    Update GameControllerTest.java

--- test/GameControllerTest.java
+++ test/GameControllerTest.java
@@ -10,36 +10,52 @@ public class GameControllerTest {
 
 	@BeforeClass
 	public static void setUp() {
-
 		board = new Board();
 		gui = new BoardGUI();
 		controller = new GameController<>(gui, board);
 	}
-
+	/**
+	 * Test placeWall method
+	 */
 	@Test
 	public void placeWallTest() {
 
-		Control.placeWall(5, 5, -1, 4, 5, 1);
-		Control.placeWall(5, 5, 0, 5, 6, 2);
+		GameController.placeWall(5, 5, -1, 4, 5, 1, 5, 6, -1, 4, 6, -1);
+		GameController.placeWall(5, 5, 0, 5, 6, 2, 5, 6, -1, 4, 6, -1);
 
-		assertEquals(true, Control.getBoard().getPosition(5, 5).hasLeftWall());
-		assertEquals(true, Control.getBoard().getPosition(4, 5).hasRightWall());
-		assertEquals(true, Control.getBoard().getPosition(5, 5).hasTopWall());
-		assertEquals(true, Control.getBoard().getPosition(5, 6).hasBottomWall());
-		assertEquals(9, Control.getCurrentPlayer().getWallCount());
-		assertEquals(1, Control.getCurrentPlayer().getMoveCount());
-
-
+		assertEquals(true, board.getPosition(5, 5).hasLeftWall());
+		assertEquals(true, board.getPosition(4, 5).hasRightWall());
+		assertEquals(true, board.getPosition(5, 5).hasTopWall());
+		assertEquals(true, board.getPosition(5, 6).hasBottomWall());
+		assertEquals(9, controller.getCurrentPlayer().getWallCount());
+		assertEquals(1, controller.getCurrentPlayer().getMoveCount());
 	}
-	public void movePawnTest(){
-	Control.movePawn(5, 6);
-	Control.movePawn(7, 6);
 
-	assertEquals(5, Control.getCurrentPlayer().getX());
-	assertEquals(6, Control.getCurrentPlayer().getY());
+	/** 
+	 * Test movePawn method.
+	 */
+	@Test
+	public void movePawnTest() {
+		GameController.movePawn(5, 6);
+		GameController.movePawn(7, 6);
 
-	Control.movePawn(1, 2);
+		assertEquals(5, controller.getCurrentPlayer().getX());
+		assertEquals(6, controller.getCurrentPlayer().getY());
 
-	assertEquals(7, Control.getCurrentPlayer().getX());
-	assertEquals(6, Control.getCurrentPlayer().getY());}
+		controller.movePawn(1, 2);
+
+		assertEquals(7, controller.getCurrentPlayer().getX());
+		assertEquals(6, controller.getCurrentPlayer().getY());
+	}
+	
+	/**
+	 * Test isValidMove method.
+	 */
+	@Test
+	public void isValidMoveTest(){
+		GameController.placeWall(5, 0, 0, 4, 0, 0, 6, 0, 0, 7, 0, 0);
+		GameController.placeWall(5, 5, 0, 5, 6, 2, 5, 6, -1, 4, 6, -1);
+
+		assertEquals(false, GameController.isValidMove(controller.getCurrentPlayer(), 4, 1));
+	}
 }
