0dc92a27f13ea037ee5924d6b3bcc1843b55b6ed
Junaid Ali Rasheed <jrasheed178@gmail.com>
2016-04-01 14:57:21 +0100

    Added more tests for the Position class

--- test/PositionTest.java
+++ test/PositionTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 
 public class PositionTest {
 	private static Position pos1;
+    private static Position pos2;
 
 	@BeforeClass
 	public static void setUp() {
 		pos1 = new Position(2, 5);
+        pos2 = new Position(5, 8);
 	}
 
 	@Test
@@ -22,17 +24,45 @@ public class PositionTest {
 	}
 
 	@Test
-	public void hasBottomWallTesst() {
+	public void hasBottomWallTest() {
 		assertEquals(false, pos1.hasBottomWall());
 		pos1.setHasBottomWall(true);
 		assertEquals(true, pos1.hasBottomWall());
 	}
 
-	@Test
-	public void setTopTest(){
+    @Test
+    public void hasLeftWallTest() {
+        assertEquals(false, pos1.hasLeftWall());
+        pos1.setHasLeftWall(true);
+        assertEquals(true, pos1.hasLeftWall());
+    }
 
+    @Test
+    public void hasRightWallTest() {
+        assertEquals(false, pos2.hasRightWall());
+        pos2.setHasRightWall(true);
+        assertEquals(true, pos2.hasRightWall());
+    }
+
+    @Test
+    public void hasTopWallTest() {
+        assertEquals(false, pos1.hasTopWall());
+        pos1.setHasTopWall(true);
+        assertEquals(true, pos1.hasTopWall());
+    }
+
+	@Test
+	public void setTopTest() {
+        assertEquals(false, pos1.isTop());
 		pos1.setTop();
 		assertEquals(true, pos1.isTop());
 
 	}
+
+    @Test
+    public void setBottomTest() {
+        assertEquals(false, pos2.isBottom());
+        pos2.setBottom();
+        assertEquals(true, pos2.isBottom());
+    }
 }
