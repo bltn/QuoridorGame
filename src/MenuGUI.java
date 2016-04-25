2c142bea4b0da8515b3c3dd9d21c842f18dacea1
Junaid Ali Rasheed <jrasheed178@gmail.com>
2016-04-25 18:51:16 +0100

    Added seperate text fields for four players

--- src/MenuGUI.java
+++ src/MenuGUI.java
@@ -1,5 +1,5 @@
+import com.sun.javafx.font.freetype.HBGlyphLayout;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -7,8 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -16,18 +15,18 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 
 public class MenuGUI extends Application {
 
     private Scene scene;
     private GridPane introPane;
     private Text introText;
-    public TextField nameTextField;
+    public TextField player1Name;
+    public TextField player2Name;
+    public TextField player3Name;
+    public TextField player4Name;
+    private HBox nameBoxRow1;
+    private HBox nameBoxRow2;
     private VBox buttonBox;
     private Button startButton;
     private Button quitButton;
@@ -38,7 +37,12 @@ public class MenuGUI extends Application {
     	LanguageFileHandler.setLanguage(SettingsGUI.language);
     	introPane = new GridPane();
     	introText = new Text("Quoridor");
-        nameTextField = new TextField("Enter your name");
+        player1Name = new TextField("Player 1");
+        player2Name = new TextField("Player 2");
+        player3Name = new TextField("Player 3");
+        player4Name = new TextField("Player 4");
+        nameBoxRow1 = new HBox();
+        nameBoxRow2 = new HBox();
     	buttonBox = new VBox();
     	introText.setId("text");
 
@@ -94,7 +98,7 @@ public class MenuGUI extends Application {
     public void setIntroPane() {
         introPane.setAlignment(Pos.CENTER);
         introPane.setHgap(25);
-        introPane.setVgap(40);
+        introPane.setVgap(5);
         introPane.add(buttonBox, 0, 1);
         introText.setTextAlignment(TextAlignment.CENTER);
         introText.setFont(Font.font("Agency FB", FontWeight.BOLD, 70));
@@ -107,7 +111,11 @@ public class MenuGUI extends Application {
     public void setButtons() {
         buttonBox.setPadding(new Insets(15, 15, 15, 15));
         buttonBox.setSpacing(10);
-        buttonBox.getChildren().add(nameTextField);
+        nameBoxRow1.setSpacing(10);
+        nameBoxRow2.setSpacing(10);
+        nameBoxRow1.getChildren().addAll(player1Name, player2Name);
+        nameBoxRow2.getChildren().addAll(player3Name, player4Name);
+        buttonBox.getChildren().addAll(nameBoxRow1, nameBoxRow2);
         buttonBox.getChildren().add(startButton);
         buttonBox.getChildren().add(multiplayerButton);
         buttonBox.getChildren().add(settingsButton);
@@ -125,7 +133,7 @@ public class MenuGUI extends Application {
         quitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                
+
             }
         });
         multiplayerButton.setPrefWidth(350);
