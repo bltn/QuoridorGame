bbe204b7f45b4ba64e476f0b8838733508d607fc
khadijapatel <khadijapatel@users.noreply.github.com>
2016-04-25 23:49:13 +0100

    settings icons

--- src/MenuGUI.java
+++ src/MenuGUI.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -29,46 +30,55 @@ public class MenuGUI extends Application {
     private Button startButton;
     private Button quitButton;
     private Button multiplayerButton;
+    private Button settingsButton;
 
     public MenuGUI() {
-    	 introPane = new GridPane();
-         introText = new Text("Quoridor");	
-         buttonBox = new VBox();
-         introText.setId("text");
-         
-         // add a icon into the start button
-         Image start = new Image(getClass().getResourceAsStream("icons/start.png"));
-         ImageView changeSizeOfStart = new ImageView(start);
-         changeSizeOfStart.setFitHeight(20);
-         changeSizeOfStart.setFitWidth(20);
-         startButton = new Button("Start",changeSizeOfStart);
-         
-         //add a icon into quit button
-         Image quit = new Image(getClass().getResourceAsStream("icons/quit.png"));
-         ImageView newQuit = new ImageView(quit);
-         newQuit.setFitHeight(20);
-         newQuit.setFitWidth(20);
-         quitButton = new Button("Quit",newQuit);
-         
-         //add a icon into the multiplayer button
-         Image multiplayer = new Image(getClass().getResourceAsStream("icons/multiplayers.png"));
-         ImageView NewMultiplayer = new ImageView(multiplayer);
-         NewMultiplayer.setFitHeight(20);
-         NewMultiplayer.setFitWidth(20);
-         multiplayerButton = new Button("Multiplayer",NewMultiplayer);
-        
-         //add background image
-         Image background = new Image(getClass().getResourceAsStream("icons/backgrounds.png"));
-         BackgroundSize size = new BackgroundSize(BackgroundSize.AUTO,
-         BackgroundSize.AUTO, false, false, true, true);
-         BackgroundImage bimg = new BackgroundImage(background,
-         BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-         BackgroundPosition.CENTER, size);
-         
-         introPane.setBackground(new Background(bimg));
-         scene = new Scene(introPane, 600, 400);
-         scene.getStylesheets().add("Theme.css");
-    }
+    	Translate.setLanguage(SettingsGUI.language);
+    	introPane = new GridPane();
+    	introText = new Text("Quoridor");
+    	buttonBox = new VBox();
+    	introText.setId("text");
+
+    	// add a icon into the start button
+    	Image start = new Image(getClass().getResourceAsStream("icons/start.png"));
+    	ImageView changeSizeOfStart = new ImageView(start);
+    	changeSizeOfStart.setFitHeight(20);
+    	changeSizeOfStart.setFitWidth(20);
+    	startButton = new Button(Translate.localMode(),changeSizeOfStart);
+
+    	//add a icon into quit button
+    	Image quit = new Image(getClass().getResourceAsStream("icons/quit.png"));
+    	ImageView newQuit = new ImageView(quit);
+    	newQuit.setFitHeight(20);
+    	newQuit.setFitWidth(20);
+    	quitButton = new Button(Translate.quit(),newQuit);
+
+    	//add a icon into the multiplayer button
+    	Image multiplayer = new Image(getClass().getResourceAsStream("icons/multiplayers.png"));
+    	ImageView NewMultiplayer = new ImageView(multiplayer);
+    	NewMultiplayer.setFitHeight(20);
+    	NewMultiplayer.setFitWidth(20);
+    	multiplayerButton = new Button(Translate.multiplayer(),NewMultiplayer);
+
+    	//add a icon into the settings button
+    	Image settings = new Image(getClass().getResourceAsStream("icons/settings.png"));
+    	ImageView newsettings = new ImageView(settings);
+    	newsettings.setFitHeight(20);
+    	newsettings.setFitWidth(20);
+    	settingsButton = new Button("Settings",newsettings);
+
+    	//add background image
+    	Image background = new Image(getClass().getResourceAsStream("icons/backgrounds.png"));
+    	BackgroundSize size = new BackgroundSize(BackgroundSize.AUTO,
+		BackgroundSize.AUTO, false, false, true, true);
+    	BackgroundImage bimg = new BackgroundImage(background,
+		BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+		BackgroundPosition.CENTER, size);
+
+    	introPane.setBackground(new Background(bimg));
+    	scene = new Scene(introPane, 600, 400);
+    	scene.getStylesheets().add(SettingsGUI.theme);
+	}
 
     @Override
     public void start(Stage primaryStage) {
@@ -85,7 +95,7 @@ public class MenuGUI extends Application {
     public void setIntroPane() {
         introPane.setAlignment(Pos.CENTER);
         introPane.setHgap(25);
-        introPane.setVgap(100);
+        introPane.setVgap(70);
         introPane.add(buttonBox, 0, 1);
         introText.setTextAlignment(TextAlignment.CENTER);
         introText.setFont(Font.font("Agency FB", FontWeight.BOLD, 70));
@@ -100,9 +110,10 @@ public class MenuGUI extends Application {
         buttonBox.setSpacing(10);
         buttonBox.getChildren().add(startButton);
         buttonBox.getChildren().add(multiplayerButton);
+        buttonBox.getChildren().add(settingsButton);
         buttonBox.getChildren().add(quitButton);
         buttonBox.setAlignment(Pos.CENTER);
-        startButton.setPrefWidth(150);
+        startButton.setPrefWidth(200);
         startButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -110,24 +121,45 @@ public class MenuGUI extends Application {
             	gui.start(new Stage());
             };
         });
-        quitButton.setPrefWidth(150);
+        quitButton.setPrefWidth(200);
         quitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.exit(0);
             }
         });
-        multiplayerButton.setPrefWidth(150);
+        multiplayerButton.setPrefWidth(200);
         multiplayerButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				GUI gui = new NetworkedBoardGUI(2);
-				GameClient client = new GameClient(gui);
-				GameServer server = new GameServer(new NetworkedGameController(new StandardBoard(2)), 2);
-				ConnectionGUI connGUI = new ConnectionGUI();
-				connGUI.start(new Stage());
-			}
+            @Override
+            public void handle(ActionEvent event) {
+                ConnectionGUI connGUI = new ConnectionGUI();
+                connGUI.start(new Stage());
+            }
         });
+        settingsButton.setPrefWidth(200);
+        final MenuGUI menuGUIReference = this;
+        settingsButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                SettingsGUI settingsGUI = new SettingsGUI(menuGUIReference);
+                settingsGUI.start(new Stage());
+            }
+        });
+    }
+
+    public void updateTheme() {
+        // Remove the currently loaded stylesheet
+        scene.getStylesheets().remove(scene.getStylesheets().get(0));
+        // Add the new stylesheet
+        scene.getStylesheets().add(SettingsGUI.theme);
+    }
+
+    public void updateLanguage() {
+        Translate.setLanguage(SettingsGUI.language);
+        startButton.setText(Translate.localMode());
+        quitButton.setText(Translate.quit());
+        multiplayerButton.setText(Translate.multiplayer());
+        settingsButton.setText(Translate.settings());
     }
 
     public static void main(String[] args) {
---
+++ src/SettingsGUI.java
@@ -0,0 +1,218 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+public class SettingsGUI extends Application{
+
+    private Scene scene;
+    private GridPane settingsPane;
+    private Text languageText;
+    private Text colourText;
+    private HBox languageBox;
+    private HBox colourBox;
+    private Button englishButton;
+    private Button frenchButton;
+    private Button spanishButton;
+    private Button chineseButton;
+    private Button standardButton;
+    private Button protanopiaButton;
+    private Button tritanopiaButton;
+    private Button deuteranopiaButton;
+    private MenuGUI menuGUI;
+
+    public static String language = "English";
+    public static String theme = "Theme.css";
+
+    public SettingsGUI(MenuGUI menuGUI) {
+        this.menuGUI = menuGUI;
+
+        settingsPane = new GridPane();
+
+        languageText = new Text("Language");
+        languageBox = new HBox();
+        languageText.setId("text");
+
+        colourText = new Text("Colour");
+        colourBox = new HBox();
+        colourText.setId("text");
+        
+        // add a icon into the English button
+    	Image english = new Image(getClass().getResourceAsStream("icons/gbr.png"));
+    	ImageView newEnglishButton = new ImageView(english);
+    	newEnglishButton.setFitHeight(20);
+    	newEnglishButton.setFitWidth(20);
+    	englishButton = new Button("English",newEnglishButton);
+    	
+        // add a icon into the French button
+    	Image french = new Image(getClass().getResourceAsStream("icons/fr.png"));
+    	ImageView newFrenchButton = new ImageView(french);
+    	newFrenchButton.setFitHeight(20);
+    	newFrenchButton.setFitWidth(20);
+    	frenchButton = new Button("Français",newFrenchButton);
+    	
+        // add a icon into the Spanish button
+    	Image spanish = new Image(getClass().getResourceAsStream("icons/sp.png"));
+    	ImageView newSpanishButton = new ImageView(spanish);
+    	newSpanishButton.setFitHeight(20);
+    	newSpanishButton.setFitWidth(20);
+    	spanishButton = new Button("Español",newSpanishButton);
+    	
+        // add a icon into the Chinese button
+    	Image chinese = new Image(getClass().getResourceAsStream("icons/cn.png"));
+    	ImageView newChineseButton = new ImageView(chinese);
+    	newChineseButton.setFitHeight(20);
+    	newChineseButton.setFitWidth(20);
+    	chineseButton = new Button("Chinese",newChineseButton);
+   
+        standardButton = new Button("Standard");
+        protanopiaButton = new Button("Protanopia");
+        tritanopiaButton = new Button("Tritanopia");
+        deuteranopiaButton = new Button("Deuteranopia");
+
+        // Background
+        Image background = new Image(getClass().getResourceAsStream("icons/backgrounds.png"));
+        BackgroundSize size = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
+        BackgroundImage bimg = new BackgroundImage(background, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, size);
+
+        settingsPane.setBackground(new Background(bimg));
+        scene = new Scene(settingsPane, 800, 400);
+        scene.getStylesheets().add(theme);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Settings");
+        setButtons();
+        setSettingsPane();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void setButtons() {
+        languageBox.setPadding(new Insets(15, 15, 15, 15));
+        languageBox.setSpacing(10);
+        languageBox.getChildren().addAll(englishButton, frenchButton, spanishButton, chineseButton);
+        languageBox.setAlignment(Pos.CENTER);
+
+        englishButton.setPrefWidth(175);
+        englishButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                language = "English";
+                Translate.setLanguage(language);
+                menuGUI.updateLanguage();
+            };
+        });
+
+        frenchButton.setPrefWidth(175);
+        frenchButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                language = "French";
+                Translate.setLanguage(language);
+                menuGUI.updateLanguage();
+            };
+        });
+
+        spanishButton.setPrefWidth(175);
+        spanishButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                language = "Spanish";
+                Translate.setLanguage(language);
+                menuGUI.updateLanguage();
+            };
+        });
+
+        chineseButton.setPrefWidth(175);
+        chineseButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                language = "Chinese";
+                Translate.setLanguage(language);
+                menuGUI.updateLanguage();
+            };
+        });
+
+        colourBox.setPadding(new Insets(15, 15, 15, 15));
+        colourBox.setSpacing(10);
+        colourBox.getChildren().addAll(standardButton, protanopiaButton, tritanopiaButton, deuteranopiaButton);
+        colourBox.setAlignment(Pos.CENTER);
+
+        standardButton.setPrefWidth(175);
+        standardButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                theme = "Theme.css";
+                updateTheme();
+                menuGUI.updateTheme();
+            };
+        });
+
+        protanopiaButton.setPrefWidth(175);
+        protanopiaButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                theme = "protanopia.css";
+                updateTheme();
+                menuGUI.updateTheme();
+            };
+        });
+
+        tritanopiaButton.setPrefWidth(175);
+        tritanopiaButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                theme = "tritanopia.css";
+                updateTheme();
+                menuGUI.updateTheme();
+            };
+        });
+
+        deuteranopiaButton.setPrefWidth(175);
+        deuteranopiaButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                theme = "deuteranopia.css";
+                updateTheme();
+                menuGUI.updateTheme();
+            };
+        });
+    }
+
+    public void setSettingsPane() {
+        settingsPane.setAlignment(Pos.CENTER);
+        settingsPane.setHgap(25);
+        settingsPane.setVgap(10);
+
+        settingsPane.add(languageBox, 0, 1);
+        languageText.setTextAlignment(TextAlignment.CENTER);
+        languageText.setFont(Font.font("Agency FB", FontWeight.BOLD, 50));
+        settingsPane.add(languageText, 0, 0, 1, 1);
+
+        settingsPane.add(colourBox, 0, 5);
+        colourText.setTextAlignment(TextAlignment.CENTER);
+        colourText.setFont(Font.font("Agency FB", FontWeight.BOLD, 50));
+        settingsPane.add(colourText, 0, 0, 1, 13);
+    }
+
+    private void updateTheme() {
+        // Remove the stylesheet currently in use
+        scene.getStylesheets().remove(scene.getStylesheets().get(0));
+        // Add the new stylesheet.
+        scene.getStylesheets().add(theme);
+    }
+}
